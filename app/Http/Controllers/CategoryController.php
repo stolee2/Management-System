5e0ec177d78876b4578d3db2d9900e34cd9615af
stolee2 <stolerrrr@gmail.com>
2025-02-16 22:46:55 +0200

    removed unused files

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,10 +7,10 @@ use App\Models\Category;
 class CategoryController extends Controller
 {
     public function index()
-{
-    $categories = Category::all();
-    return response()->json($categories);
-}
+    {
+        $categories = Category::all();
+        return response()->json($categories);
+    }
 
 
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,11 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(_DIR_))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: _DIR_.'/../routes/web.php',
-        api: _DIR_.'/../routes/api.php',
-        commands: _DIR_.'/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
